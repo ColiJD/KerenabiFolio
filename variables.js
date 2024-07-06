c7f01df6c07b021e836bfdc72b2c5fbbd19dc420
ColiJD <colindresj9@gmail.com>
2024-07-06 22:15:10 +0000

    vite

--- variables.js
+++ variables.js
@@ -16,20 +16,20 @@ const handlerBarsContext = {
         archivos: archivos,
         redes: redes,
     },
-	// "/page/donaciones.html": {
-	// 	title: "Donaciones",
-	// 	logo: "https://acoes.org/wp-content/uploads/2023/10/LG-FUNDACION-BN-2-2.png",
-	// 	navegacion: navegacion,
-	// 	btnApadrina: btnApadrina,
-	// 	card: card,
-	// 	phones: phones,
-	// 	footerNavegacion: footerNavegacion,
-	// 	redes: redes,
-	// },
+    // "/page/donaciones.html": {
+    // 	title: "Donaciones",
+    // 	logo: "https://acoes.org/wp-content/uploads/2023/10/LG-FUNDACION-BN-2-2.png",
+    // 	navegacion: navegacion,
+    // 	btnApadrina: btnApadrina,
+    // 	card: card,
+    // 	phones: phones,
+    // 	footerNavegacion: footerNavegacion,
+    // 	redes: redes,
+    // },
 };
 
 const pageContext = (page) => {
-	const context = { ...handlerBarsContext[page] };
-	return context;
+    const context = { ...handlerBarsContext[page] };
+    return context;
 };
 export default pageContext;
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import * as glob from 'glob';
 import path, { resolve } from 'node:path';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import htmlPurge from 'vite-plugin-purgecss';
-import handlebars from 'vite-plugin-handlebars';
+import handlebars from 'vite-plugin-handlebars'
 import handlerBarsContext from './variables.js';
 
 export default defineConfig({
